83caed0fa2460338e01f41471c549a943b77808a
Tanmoy Chatterjee <91433845+iamTANMOY7@users.noreply.github.com>
2024-12-06 21:16:38 +0530

    commit

--- recovery.py
+++
@@ -1,4 +0,0 @@
-from vscode_deleted_file_recovery import Recover
-Recover.print_files(search_term="/location of the folder/") 
-from vscode_deleted_file_recovery import Recover
-Recover.restore_files(search_term="/location of the folder/")
