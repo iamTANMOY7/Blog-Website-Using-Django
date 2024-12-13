2588bbdce2810ec52ee75cc1f23adb65fa97de8a
Tanmoy Chatterjee <91433845+iamTANMOY7@users.noreply.github.com>
2024-12-13 22:23:11 +0530

    update

--- codehub/settings.py
+++ codehub/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "static/"
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
--- codehub/wsgi.py
+++ codehub/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ecom_proj project.
+WSGI config for codehub project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
